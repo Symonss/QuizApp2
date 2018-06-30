1d546a071420d886b08fd5c2baa59fd677d2f9ee
symonss <somborisymonss@gmail.com>
2018-06-30 14:07:36 +0300

    the second questions method complete

--- app/src/main/java/com/example/symons/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/symons/quiz_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.symons.quiz_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -16,63 +17,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int total=0; //for the total amount
+    int total = 0; //for the total amount
 
-    int q1points=0;
+    int q1points = 0;
+    int q2points = 0;
 
-
-    public void submit (View view){
-        quiz1();
-
-        Toast.makeText(this,"You got " + total+ " questions correct",Toast.LENGTH_SHORT).show();
-    }
 
     //Question1
-    public void quiz1(){
-        RadioGroup answers =  findViewById(R.id.ans1);
+    public void quiz1() {
+        RadioGroup answers = findViewById(R.id.ans1);
         switch (answers.getCheckedRadioButtonId()) {
             case R.id.q1ans:
-                 findViewById(R.id.q1ans);
+                findViewById(R.id.q1ans);
                 //calculate();
-                q1points=1;
+                q1points = 1;
                 break;
             default:
-                 findViewById(R.id.q1ans);
+                findViewById(R.id.q1ans);
 
         }
     }
 
     //Question2
-    public void quiz2(){
-        RadioGroup answers =  findViewById(R.id.ans1);
-        switch (answers.getCheckedRadioButtonId()) {
-            case R.id.q1ans:
-                findViewById(R.id.q1ans);
-                //calculate();
-                q1points=1;
-                break;
-            default:
-                findViewById(R.id.q1ans);
-
+    public void quiz2() {
+        EditText q2ans = findViewById(R.id.txtq2);
+        String ans2 = q2ans.getText().toString();
+        if (ans2.equalsIgnoreCase("Block Chain")) {
+            q2points = 1;
         }
     }
 
 
-
-
-
-
-
-
-    /*
-    This method is for calculating the total amount of marks scored
-    this method is called for every question.
-     */
-        public void calculate(){
-       total=q1points;
-
-
+    public void submit(View view) {
+        quiz1();
+        quiz2();
+        total=(q1points + q2points);
+        Toast.makeText(this, "You got " +total + " questions correct", Toast.LENGTH_SHORT).show();
     }
-
 
 }
